98dddb2b3a2c25e7ad7fef34a582394b6d3f7306
R-T-B <21GunSoftware@comcast.net>
2022-11-05 16:27:57 -0700

    Commit source.

---
+++ KSSL.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+
+namespace KSSL
+{
+    internal class KSSL
+    {
+        private static void Main(string[] args)
+        {
+            string fileName = "../KSP_x64.exe";
+            string text = "";
+            foreach (string str in args)
+            {
+                text = text + " " + str;
+            }
+            if (!KSSL.launched)
+            {
+                new Process
+                {
+                    StartInfo =
+                    {
+                        UseShellExecute = false,
+                        WorkingDirectory = KSSL.HomeDir + "/../",
+                        FileName = fileName,
+                        CreateNoWindow = true,
+                        Arguments = text
+                    }
+                }.Start();
+                KSSL.launched = true;
+            }
+        }
+
+        public static string HomeDir
+        {
+            get
+            {
+                if (KSSL.homeDir == null)
+                {
+                    KSSL.homeDir = Uri.UnescapeDataString(new UriBuilder(Assembly.GetExecutingAssembly().CodeBase).Path);
+                    KSSL.homeDir = Path.GetDirectoryName(KSSL.homeDir);
+                }
+                return KSSL.homeDir;
+            }
+        }
+
+        public static bool launched;
+
+        private static string homeDir;
+    }
+}
